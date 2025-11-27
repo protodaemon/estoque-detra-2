9161e020f92dccb74d7f67d2f316f303efe2af2f
lionh33 <ricardo.slf2112@gmail.com>
2025-11-27 12:24:54 -0300

    mais configurações pra facilitar deploy

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-
 import path from 'path'
 
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // adiciona o alias @ para src
+// Note que agora exportamos uma FUNÇÃO que recebe 'mode'
+export default defineConfig(({ mode }) => {
+  // Carrega as variáveis de ambiente baseadas no modo (development ou production)
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  //base: '/estoque_patrimonio/patrimonio/dist/',  // Isso gera caminhos relativos em vez de absolutos //prod
+    // Agora a variável 'env' existe e podemos usar o VITE_BASE_PATH
+    base: env.VITE_BASE_PATH,
+  }
 })
